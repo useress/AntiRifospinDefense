83121b5d5e9268ce1a6f5a57fd45e74c1d951510
useress <user233013@gmail.com>
2023-12-16 15:05:34 +0300

    second commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from telethon.sync import TelegramClient, events
 from random import randint
 
-api_id = 25655106
-api_hash = 'd7293f5b1b6277b7b4230dc71fcab1de'
-phone_number = '+375256081568'
+# '~' == your info
+api_id = '~'
+api_hash = '~'
+phone_number = '~'
 client = TelegramClient(phone_number, api_id, api_hash)
-chat_id = -1001855909836 
+chat_id = '~' 
 
 ANSWERS = ['ну ой ой ой', '🤡', '"я больше не буду парить вам мозги"', 'топ 100 адекватности, просто удача -100',
            'надоело гэта слухаць', 'fired.gif', 'emoji-drop.gif', 'brainbang.gif', 'kekw.gif']
@@ -16,10 +17,10 @@ def generate_answer(mes: str):
 @client.on(events.NewMessage(chats=chat_id))
 async def handle_new_message(event: events):
     sender = await event.get_sender()
-    if sender.id == 948419219:
+    if sender.id == '~':
         ANSWERS.append(event.message.text[::-1])
         x = randint(0, len(ANSWERS) - 1)
-        
+
         if '.gif' in ANSWERS[x]:
              await event.reply(file=ANSWERS[x])
         else:
