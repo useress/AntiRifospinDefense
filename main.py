d0a702e3adf8d0552f4083d0ff8e3f7af2af243e
useress <user233013@gmail.com>
2023-12-17 12:54:38 +0300

    6 commit

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from telethon.sync import TelegramClient, events
 from random import randint
+import asyncio
 
 # '~' == your info
-api_id = '~'
-api_hash = '~'
-phone_number = '~'
+api_id = 25655106
+api_hash = 'd7293f5b1b6277b7b4230dc71fcab1de'
+phone_number = '+375256081568'
 client = TelegramClient(phone_number, api_id, api_hash)
-chat_id = int(input())
-friend_id = int(input())
+f = open('info.txt', 'r')
+a = f.read().split()
+chat_id = int(a[0])
+friend_id = int(a[1])
 
-ANSWERS = ['ну ой ой ой', '🤡', '"я больше не буду парить вам мозги"', 'топ 100 адекватности, просто удача -100',
+ANSWERS = ['ну ой ой ой', '🗿', '"я больше не буду парить вам мозги"', 'топ 100 адекватности, просто удача -100',
            'надоело гэта слухаць', 'fired.gif', 'emoji-drop.gif', 'brainbang.gif', 'kekw.gif']
 
 @client.on(events.NewMessage(chats=chat_id))
@@ -17,14 +20,19 @@ async def handle_new_message(event: events):
     sender = await event.get_sender()
     if sender.id == friend_id:
         ANSWERS.append(event.message.text[::-1])
-        x = randint(0, len(ANSWERS) - 1)
-
-        if '.gif' in ANSWERS[x]:
-             await event.reply(file=ANSWERS[x])
+        mes = ANSWERS[randint(0, len(ANSWERS) - 1)]
+        del ANSWERS[-1]
+        if '.gif' in mes:
+             await event.reply(file=mes)
         else:
-             await event.reply(ANSWERS[x])
+             await event.reply(mes)
+        
 
-if __name__ == '__main__':
-    
-    client.start()   
-    client.run_until_disconnected()
+async def main():
+    async with client:
+        client.add_event_handler(handle_new_message, events.NewMessage(chats=chat_id))
+        await client.run_until_disconnected()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
